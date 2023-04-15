e3b85e12ee1f29138fa8fd8e61f5c4471ecdc6fe
Ankit Patil <22amtics124@gmail.com>
2023-04-15 15:12:43 +0530

    included maintemplate.js

--- TEXCODE/index.js
+++ TEXCODE/index.js
@@ -1,28 +1,3 @@
-// ----------------------------Sidebar----------------------------
-let droplist = document.getElementById("catagory-list");
-let ham = document.getElementById("hamburger");
-let sidebar = document.getElementsByClassName("sidebar")[0];
-droplist.style.display = "none";
-sidebar.style.display = "none";
-function drop() {
-    if(droplist.style.display == "none")
-    {
-        droplist.style.display = "block"; 
-      }
-    else{
-      droplist.style.display = "none";
-    }
-}
-
-function showsidebar() {
-    console.log("hii")
-    if (sidebar.style.display == "none") {
-      sidebar.style.display = "block";
-    } else {
-      sidebar.style.display = "none";
-    }
-}
-
 // ----------------------------Copy----------------------------
 function copy1() {
     console.log("HIi");
